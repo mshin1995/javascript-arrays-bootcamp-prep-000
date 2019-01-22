2932d4444e068e0f92073a357abc2ad8979c78ee
Matthew Shin <mshin1995@gmail.com>
2019-01-22 06:15:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,8 +24,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
-function destructivelyRemoveElementFromEndOfArray() {
-  
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
 }
 function removeElementFromEndOfArray() {
   
